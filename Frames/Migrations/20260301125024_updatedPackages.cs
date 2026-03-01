8af66a554d8d61a19dcbb878e74f76daf3beb66d
rk25412 <58212106+rk25412@users.noreply.github.com>
2026-03-01 21:03:06 +0800

    .NET Version update (#6)

---
+++ Frames/Migrations/20260301125024_updatedPackages.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Frames.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedPackages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BillingSummary_BillingId",
+                table: "BillingSummary");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BillingSummary_BillingId",
+                table: "BillingSummary",
+                column: "BillingId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BillingSummary_BillingId",
+                table: "BillingSummary");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BillingSummary_BillingId",
+                table: "BillingSummary",
+                column: "BillingId");
+        }
+    }
+}
