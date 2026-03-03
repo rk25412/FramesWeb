b57367bdc4560eb851b56fd0aaae9173515f45d9
ravi <rk25412@gmail.com>
2026-03-03 19:16:54 +0530

    Limited the text fields to 50

--- Frames/Entities/BillingItem.cs
+++ Frames/Entities/BillingItem.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Frames.Entities;
 
 public class BillingItem : EntityBase
 {
+    [MaxLength(50)]
     public string? ItemName { get; set; }
     public decimal Rate { get; set; }
     public long BillingId { get; set; }
